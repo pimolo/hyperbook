4b75c8b82ed05a9ccedde0db1c95f807ff788563
Rémi Andrieux <pimolo10300@gmail.com>
2016-05-23 14:14:09 +0200

    Add simple controllers and templates for the front

---
+++ src/AppBundle/Controller/Front/BookController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Controller\Front;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Book;
+use AppBundle\Form\BookType;
+
+/**
+ * Book controller.
+ *
+ * @Route("/book")
+ */
+class BookController extends Controller
+{
+    /**
+     * Lists all Book entities.
+     *
+     * @Route("/", name="book_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $books = $em->getRepository('AppBundle:Book')->findAll();
+
+        return $this->render('AppBundle:Front:Book/index.html.twig', array(
+            'books' => $books,
+        ));
+    }
+
+    /**
+     * Creates a new Book entity.
+     *
+     * @Route("/new", name="book_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $book = new Book();
+        $form = $this->createForm('AppBundle\Form\BookType', $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($book);
+            $em->flush();
+
+            return $this->redirectToRoute('book_show', array('id' => $book->getId()));
+        }
+
+        return $this->render('AppBundle:Front:Book/new.html.twig', array(
+            'book' => $book,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Book entity.
+     *
+     * @Route("/{id}", name="book_show")
+     * @Method("GET")
+     */
+    public function showAction(Book $book)
+    {
+        $deleteForm = $this->createDeleteForm($book);
+
+        return $this->render('AppBundle:Front:Book/show.html.twig', array(
+            'book' => $book,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Book entity.
+     *
+     * @Route("/{id}/edit", name="book_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Book $book)
+    {
+        $deleteForm = $this->createDeleteForm($book);
+        $editForm = $this->createForm('AppBundle\Form\BookType', $book);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($book);
+            $em->flush();
+
+            return $this->redirectToRoute('book_edit', array('id' => $book->getId()));
+        }
+
+        return $this->render('AppBundle:Front:Book/edit.html.twig', array(
+            'book' => $book,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Book entity.
+     *
+     * @Route("/{id}", name="book_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Book $book)
+    {
+        $form = $this->createDeleteForm($book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($book);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('book_index');
+    }
+
+    /**
+     * Creates a form to delete a Book entity.
+     *
+     * @param Book $book The Book entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Book $book)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('book_delete', array('id' => $book->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
